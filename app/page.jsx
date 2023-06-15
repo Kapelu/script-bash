b31052d1fbdaf10416b21a6cdde3c6a0f1f6fbc0
kapelu <ddaniel_calderon@hotmail.com>
2023-06-14 23:07:53 -0300

    Update 14-06-2023

--- app/page.jsx
+++ app/page.jsx
@@ -3,57 +3,35 @@ export default function Home() {
 		<>
 			<section className='seccion'>
 				<article className='contenedor'>
-					<h1 className='titulo' >Home</h1>
-					<p className='descripcion' >
-						React es una biblioteca. Te permite agrupar componentes,
+					<h1 className='titulo'>¿Qué es React y para qué sirve?</h1>
+					<p className='descripcion'>
+						React JS es una librería de código abierto que se
+						utiliza principalmente para construir interfaces; es
+						decir, es una tecnología enfocada en la interactividad.
+						Está orientada al front-end (parte del software que el
+						usuario utiliza) y su adopción ha ido aumentado en los
+						últimos años.
+						<br />
+						<br />
+						React JS se presenta como una excelente alternativa para
+						hacer todo tipo de aplicaciones web, SPA (Single Page
+						Application) y hasta aplicaciones para dispositivos
+						móviles. Se destaca por contar con un completo
+						ecosistema de módulos, herramientas y componentes que
+						posibilitan desarrollar funcionalidades complejas en
+						poco tiempo. En ese sentido, React ofrece un entorno de
+						trabajo robusto para programar usando el lenguaje
+						JavaScript. Además, brinda ciertas facilidades para
+						generar interfaces de usuario dinámicas.
+						<br />
+						<br />
+						Te permite agrupar componentes,
 						pero no prescribe cómo hacer el enrutamiento y la
 						obtención de datos. Para construir una aplicación
 						completa en React, recomendamos un framework full-stack
 						de React como Next.js
-						<br/>
-
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium, laborum dicta placeat, nam dolore corrupti ea eveniet quia quos enim impedit aliquid, explicabo animi. At fugit aut minima velit molestiae odio nulla rerum quaerat repudiandae earum, laborum pariatur vel voluptatum libero? Quae explicabo earum, libero possimus natus reprehenderit optio quas neque, soluta illo dolorem repellat inventore labore vel animi deleniti numquam! Illo, obcaecati doloremque nam perferendis amet a beatae ipsa quia, ullam id ipsum consectetur eum. Totam blanditiis vel adipisci iure sed, sint a eum rem ut dolores placeat aspernatur illo expedita ad quo harum reprehenderit nam quis neque?
 					</p>
-					<p className='descripcion' >
-						React es una biblioteca. Te permite agrupar componentes,
-						pero no prescribe cómo hacer el enrutamiento y la
-						obtención de datos. Para construir una aplicación
-						completa en React, recomendamos un framework full-stack
-						de React como Next.js
-						<br/>
-
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium, laborum dicta placeat, nam dolore corrupti ea eveniet quia quos enim impedit aliquid, explicabo animi. At fugit aut minima velit molestiae odio nulla rerum quaerat repudiandae earum, laborum pariatur vel voluptatum libero? Quae explicabo earum, libero possimus natus reprehenderit optio quas neque, soluta illo dolorem repellat inventore labore vel animi deleniti numquam! Illo, obcaecati doloremque nam perferendis amet a beatae ipsa quia, ullam id ipsum consectetur eum. Totam blanditiis vel adipisci iure sed, sint a eum rem ut dolores placeat aspernatur illo expedita ad quo harum reprehenderit nam quis neque?
-					</p>
-					<p className='descripcion' >
-						React es una biblioteca. Te permite agrupar componentes,
-						pero no prescribe cómo hacer el enrutamiento y la
-						obtención de datos. Para construir una aplicación
-						completa en React, recomendamos un framework full-stack
-						de React como Next.js
-						<br/>
-
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium, laborum dicta placeat, nam dolore corrupti ea eveniet quia quos enim impedit aliquid, explicabo animi. At fugit aut minima velit molestiae odio nulla rerum quaerat repudiandae earum, laborum pariatur vel voluptatum libero? Quae explicabo earum, libero possimus natus reprehenderit optio quas neque, soluta illo dolorem repellat inventore labore vel animi deleniti numquam! Illo, obcaecati doloremque nam perferendis amet a beatae ipsa quia, ullam id ipsum consectetur eum. Totam blanditiis vel adipisci iure sed, sint a eum rem ut dolores placeat aspernatur illo expedita ad quo harum reprehenderit nam quis neque?
-					</p>
-					<p className='descripcion' >
-						React es una biblioteca. Te permite agrupar componentes,
-						pero no prescribe cómo hacer el enrutamiento y la
-						obtención de datos. Para construir una aplicación
-						completa en React, recomendamos un framework full-stack
-						de React como Next.js
-						<br/>
-
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium, laborum dicta placeat, nam dolore corrupti ea eveniet quia quos enim impedit aliquid, explicabo animi. At fugit aut minima velit molestiae odio nulla rerum quaerat repudiandae earum, laborum pariatur vel voluptatum libero? Quae explicabo earum, libero possimus natus reprehenderit optio quas neque, soluta illo dolorem repellat inventore labore vel animi deleniti numquam! Illo, obcaecati doloremque nam perferendis amet a beatae ipsa quia, ullam id ipsum consectetur eum. Totam blanditiis vel adipisci iure sed, sint a eum rem ut dolores placeat aspernatur illo expedita ad quo harum reprehenderit nam quis neque?
-					</p>
-					<p className='descripcion' >
-						React es una biblioteca. Te permite agrupar componentes,
-						pero no prescribe cómo hacer el enrutamiento y la
-						obtención de datos. Para construir una aplicación
-						completa en React, recomendamos un framework full-stack
-						de React como Next.js
-						<br/>
-
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium, laborum dicta placeat, nam dolore corrupti ea eveniet quia quos enim impedit aliquid, explicabo animi. At fugit aut minima velit molestiae odio nulla rerum quaerat repudiandae earum, laborum pariatur vel voluptatum libero? Quae explicabo earum, libero possimus natus reprehenderit optio quas neque, soluta illo dolorem repellat inventore labore vel animi deleniti numquam! Illo, obcaecati doloremque nam perferendis amet a beatae ipsa quia, ullam id ipsum consectetur eum. Totam blanditiis vel adipisci iure sed, sint a eum rem ut dolores placeat aspernatur illo expedita ad quo harum reprehenderit nam quis neque?
-					</p>
+					
 				</article>
 			</section>
 		</>
