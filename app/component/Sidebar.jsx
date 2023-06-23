0b757b102a40d1e9e371b6f5246c574944bf8aba
kapelu <ddaniel_calderon@hotmail.com>
2023-06-22 22:52:42 -0300

    Update 22-06-2023

--- app/component/Sidebar.jsx
+++ app/component/Sidebar.jsx
@@ -6,8 +6,8 @@ export default function Sidebar() {
 			<aside className=''>
                 <Link href='./'> Home  </Link>
                 <Link href='./componentes'> Componentes </Link>
-                {/*<Link href='./clase2'> Clase 2 </Link>
-                <Link href='./clase3'> Clase 3 </Link>
+                <Link href='./propiedades'> Propiedades </Link>
+               {/* <Link href='./clase3'> Clase 3 </Link>
                 <Link href='./clase4'> Clase 4 </Link> */}
 			</aside>
 		</>
