9bb8776829f0de046b89c7a4d8685cc19fa5dc52
kapelu <ddaniel_calderon@hotmail.com>
2023-07-13 23:37:26 -0300

    Update 13-06-2023

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Componente from './components/Componente'
+/* import Componente from './components/Componente.js' */
+
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<h1>Welcome to React</h1>
+				</header>
+				<section>
+					<h2>Componente</h2>
+					<Componente msg='Hola, Soy un componente' />
+					<br />
+					<br />
+				</section>
+				<hr />
+				<footer className='footer'>
+					<small>
+						Sitio diseñado y desarrollado por &nbsp;
+						<a
+							className='link'
+							href='https://github.com/Kapelu'
+							target='_blank'
+							rel='noopener'
+						>
+							@Kapelu
+						</a>
+					</small>
+				</footer>
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ src/components/Componente.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Componente(props) {
+	return (
+		<div className='componente'>
+            <h3>{props.msg}</h3>
+        </div>
+	)
+}
