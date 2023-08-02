4b2e105748a04151074f1c6c63ec41fdd6c59c06
kapelu <ddaniel_calderon@hotmail.com>
2023-08-02 00:57:12 -0300

    Update 02-07-2023

--- app/components/Estilos.jsx
+++ app/components/Estilos.jsx
@@ -1,13 +1,22 @@
 'use client'
 import './style.css'
+import './estilos.css'
 
 export default function Estilos() {
 	return (
-		<div className='ejercicio'>
-			<h3>
-				<i>&#39;Estilos&#39;</i>
-			</h3>
+		<div className='ejercicio estilos'>
+			<article className='bg-react'>
+				<span>
+					<i>Estilos en hojas &#39;CSS externas&#39;</i>
+				</span>
+			</article>
+			<article className='bg-react' style={{borderRadius:'.5rem', backgroundColor:'#b8b8b8'}}>
+				<span >
+					<i>Estilos en línea &#39;style=&#123;&#125;&#39;</i>
+				</span>
+			</article>
 			<br />
-        </div>
+			<br />
+		</div>
 	)
 }
