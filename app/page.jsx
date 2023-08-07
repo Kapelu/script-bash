48225e8ed5f4931ac2c9107bfe7b91edf1c4bf86
kapelu <ddaniel_calderon@hotmail.com>
2023-08-06 21:03:39 -0300

    Update 06-08-2023

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
 	return (
 		<>
-			<section>
-				<h1>Home page</h1>
-			</section>
+			<article className={styles.home}>
+				<h1 style={{textAlign:'center'}}>Home Page</h1>
+				
+			</article>
 		</>
 	)
 }
--- app/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-	return (
-		<>
-			<aside>
-				<h1>home</h1>
-			</aside>
-		</>
-	)
-}
