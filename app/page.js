90e29dbd7cb5ee593b7ff4567af8de7383ef1aa1
kapelu <ddaniel_calderon@hotmail.com>
2023-08-27 23:55:53 -0300

    Update 27-08-2023

---
+++ app/page.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Inicio from './inicio/page'
+import Contacto from './contacto/page'
+
+export default function Home() {
+  return (
+    <>
+			<section id='inicio' className='home'>
+				<Inicio />
+			</section>
+      <section id='contacto' className='contacto'>
+				<Contacto />
+			</section>
+
+			{/* <section id='cosmetica' className=''>
+				<Bazar />
+			</section> */}
+			
+		</>
+  )
+}
