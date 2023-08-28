ca0123e9266ae131d973327210ebbadfef20523c
kapelu <ddaniel_calderon@hotmail.com>
2023-08-28 00:13:07 -0300

    Update 28-08-2023

---
+++ app/inicio/page.jsx
@@ -0,0 +1,19 @@
+'use client'
+import './style.css'
+
+export default function Inicio() {
+	
+	return (
+		<article className='hero-image-cosmetica'>
+			<aside className='hero-image-opacity'>
+				<div className='container'>
+					<h2 className='section-title'>Cosmeticos</h2>
+
+					<h2>Hola</h2>
+
+					
+				</div>
+			</aside>
+		</article>
+	)
+}
