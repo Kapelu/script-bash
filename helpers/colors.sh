a9176a29a799a3e297f4111819d78f8d1d3b66b5
kapelu <ddaniel_calderon@hotmail.com>
2026-02-06 01:28:40 -0300

    ✔ Update 06-02-2026

---
+++ helpers/colors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+blue="\e[0;36m"
+green="\e[0;32m"
+red="\e[1;31m"
+red_bg="\e[1;41m"
+yellow="\e[0;33m"
+close_color="\e[0m"
+
+show_log() {
+  echo -e "\n-----> $1"
+}
+show_info_log() {
+  echo -e "\n$blue-----> $1 $close_color"
+}
+show_success_log() {
+  echo -e "\n$green-----> $1 $close_color"
+}
+show_warning_log() {
+  echo -e "\n$yellow-----> $1 $close_color"
+}
+show_error_log() {
+  echo -e "\n$red_bg ERROR: $1 $close_color"
+}
+
+show_info_input() {
+  echo -ne "\n$blue-----> $1 $close_color"
+}
